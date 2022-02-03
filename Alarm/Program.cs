bd6a7aed4cb0c93725a75fa7a5c4a036f574521b
le pavec malo <malo.lepavec@gmail.com>
2022-02-03 18:01:37 +0100

    Ajoutez des fichiers projet.

---
+++ Alarm/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using System.Threading.Tasks;
+
+bool Clock(DateTime date)
+{
+    
+    DateTime actually;
+    do
+    {
+        actually = DateTime.Now;
+        TimeSpan ts = date - actually;
+        Console.WriteLine("il reste : " + ts.Seconds.ToString() + " secondes");
+    } while (actually < date);
+
+    return true;
+}
+
+void ring()
+{
+    Clock(DateTime.Now.AddMinutes(1));
+    Console.WriteLine("Il faut se réveiller");
+}
+
+ring();
+
+
